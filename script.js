bd020f51d17390b5fcc6605de4041d2fb74abfe2
bollowatsch <stephan.bollauf@gmail.com>
2022-08-18 13:31:50 +0200

    implement function, which returns result of one round

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const sign = ["rock", "paper", "scissor"];
+const sign = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return sign[Math.floor(Math.random() * 3)];
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let res = null;
+  if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") res = "w";
+  else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") res = "l";
+  else res = "d";
+
+  return res === "w" ? `You win! ${playerSelection} beats ${computerSelection}!` : res === "l" ? `You lose! ${playerSelection} loses to ${computerSelection}!` : `It's a draw! You both chose ${playerSelection}!`;
+}
