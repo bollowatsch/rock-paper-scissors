3729225c9e40af25379630bc1440df2a51cec7cd
bollowatsch <stephan.bollauf@gmail.com>
2022-08-19 17:45:19 +0200

    implement game function, which plays 5 rounds with userinput against computer chosen input

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice() {
   return sign[Math.floor(Math.random() * 3)];
 }
 
-//this function simulates one round against the computer and returns, whether the player won, or not
+//playRound simulates one round against the computer and returns, whether the player won, lost or if it's a draw
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   //res is the result as seen by the human player: w=won, l=lost, d=draw
@@ -28,14 +28,23 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+//the game function plays 5 rounds and prints the summary of results afterwards
 function game() {
-  let winCounter, loseCounter, drawCounter = 0;
-  for (let i = 0; i <= 5; i++) {
+  let winCounter = 0, lossCounter = 0, drawCounter = 0;
+  for (let i = 0; i < 5; i++) {
     let userInput = prompt("Choose your sign! (rock, paper, scissors)");
     while (!sign.includes(userInput.toLowerCase())) {
       userInput = prompt("Invalid input! Please choose your sign again! (rock, paper, scissors)");
-      let currentRes = playRound(userInput, getComputerChoice);
     }
+    let currentRes = playRound(userInput, getComputerChoice());
+    currentRes === "w" ? winCounter++ : currentRes === "l" ? lossCounter++ : drawCounter++;
   }
-  if 
+  console.log(`Game statistics:\n
+    Wins: ${winCounter}\n
+    Losses: ${lossCounter}\n
+    Draws: ${drawCounter}`
+  );
+  if (winCounter > lossCounter) console.log("YOU WON!");
+  else if (winconter < lossCounter) console.log("YOU LOST!");
+  else console.log("IT'S A DRAW!");
 }
